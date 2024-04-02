5903757d42db12bcc1c03efa1d7c5f26a365afbb
Amarjit Yanglem <aayanglem@gmail.com>
2024-04-02 11:30:14 +0530

    Updated: Mobile Home UI adjusted

--- app/_components/Hero1.jsx
+++ app/_components/Hero1.jsx
@@ -3,7 +3,7 @@ import Constant from "../_utils/Constant";
 
 function Hero1() {
   return (
-    <section className="bg-gray-900 text-white">
+    <section className="bg-gray-900 h-screen text-white">
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex">
         <div className="mx-auto max-w-3xl text-center">
           <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
