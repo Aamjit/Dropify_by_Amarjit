d6a27a8d8757bc7513d369945f04171299e8deec
Amarjit Yanglem <aayanglem@gmail.com>
2024-04-03 16:55:05 +0530

    Fix: added hero description

--- app/_utils/Constant.js
+++ app/_utils/Constant.js
@@ -1,5 +1,8 @@
-export default{
-    authPageImage: "https://images.unsplash.com/photo-1625014618427-fbc980b974f5?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    signInBanner: `Sign in to continue to your profile dashboard. If your new don't forget to sign up`,
-    signUpBanner: `Sign up to explore our file sharing wonders🌍 and start uploading your own files!`,
-}
+export default {
+  bannerDesc:
+    "Choose and upload your file you want to share with your friends and family, create a link to share it with them. Add a password to keep it a secret🤫.",
+  authPageImage:
+    "https://images.unsplash.com/photo-1625014618427-fbc980b974f5?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  signInBanner: `Sign in to continue to your profile dashboard. If your new don't forget to sign up`,
+  signUpBanner: `Sign up to explore our file sharing wonders🌍 and start uploading your own files!`,
+};
