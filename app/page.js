a13677297527c36ca2043d710c79dc516fcc1437
Amarjit Yanglem <aayanglem@gmail.com>
2024-04-03 16:48:42 +0530

    Update: added announcement

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
-"use client"
+"use client";
 import Header from "./_components/Header";
 import Hero1 from "./_components/Hero1";
+import Announcement from "./_components/Announcement";
 
 export default function Home() {
   return (
     <div className="bg-gray-900 text-white m-auto h-screen min-h-screen">
+      <Announcement />
       <Header />
       <Hero1 />
     </div>
