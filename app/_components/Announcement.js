7bea403b9d387058306b464b5ca9c3b885cae320
Amarjit Yanglem <aayanglem@gmail.com>
2024-04-05 15:53:12 +0530

    Update: new announcement

--- app/_components/Announcement.js
+++ app/_components/Announcement.js
@@ -2,42 +2,54 @@
 import React, { useState } from "react";
 
 function Announcement() {
-  const [showAnnounce, setShowAnnounce] = useState(true);
+	const [showAnnounce, setShowAnnounce] = useState(true);
 
-  return (
-    showAnnounce && (
-      <div>
-        <div className="flex items-center justify-between gap-4 bg-indigo-600 px-4 py-3 text-white">
-          <p className="text-sm font-mono">
-            📢 Please be advised that this apllication is a work in progress and
-            you might run into few issues.
-            {/* <a href="#" className="inline-block underline">
-            Check out this new course!
-          </a> */}
-          </p>
+	const announcements = [
+		{
+			msg: "📢 Please be advised that this apllication is a work in progress and you might run into few issues.",
+		},
+		{
+			msg: "📢 We are working on providing the most secure ways for you to access your information.",
+		},
+	];
 
-          <button
-            aria-label="Dismiss"
-            className="shrink-0 rounded-lg bg-black/10 p-1 transition hover:bg-black/20"
-            onClick={() => setShowAnnounce(false)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
-    )
-  );
+	return (
+		showAnnounce && (
+			<div>
+				<div className="flex items-center justify-between gap-4 bg-indigo-600 px-4 py-3 mb-2 text-white">
+					<div>
+						{announcements?.map((item) => (
+							<p className="inline-block text-sm font-mono my-2 border-b">
+								{item?.msg}
+							</p>
+						))}
+					</div>
+
+					<button
+						aria-label="Dismiss"
+						className="shrink-0 rounded-lg bg-black/10 p-1 transition hover:bg-black/20"
+						onClick={(e) => {
+							e.preventDefault();
+							setShowAnnounce(false);
+						}}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-5 w-5"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fillRule="evenodd"
+								d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+								clipRule="evenodd"
+							/>
+						</svg>
+					</button>
+				</div>
+			</div>
+		)
+	);
 }
 
 export default Announcement;
