5af0ba8bd4374db071b74cd7fad2bb096411c7a7
Amarjit Yanglem <aayanglem@gmail.com>
2024-04-07 11:02:10 +0530

    Fix: redirect auth page

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -50,7 +50,8 @@ function Header() {
 									<a
 										className="rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white shadow
                             transition hover:bg-blue-800"
-										href="/sign-up"
+										// href="/sign-up"	Development
+										href="/uploads" // Production
 									>
 										Get Started
 									</a>
